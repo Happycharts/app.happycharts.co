a6c0b41289d84c0f71f6b313abd957ff0d78a864
James Bohrman <james@getoasis.io>
2024-08-07 01:39:22 -0400

    push

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 "use client"
+import { useEffect } from "react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -10,12 +11,19 @@ export const dynamic = "force-dynamic";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans", weight: ["400","700"] });
 
-ServiceBell("init", "b951bf1ae1c8405b8f7a47ae2a153512");
+// Define the type for the ServiceBell function
+declare function ServiceBell(action: string, apiKey: string): void;
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  useEffect(() => {
+    // Use a type assertion to tell TypeScript that ServiceBell is a function
+    (ServiceBell as any)("init", "b951bf1ae1c8405b8f7a47ae2a153512");
+  }, []);
+
   return (
     <ClerkProvider afterSignOutUrl={"https://www.happybase.co"} appearance={{
       elements: {
@@ -32,4 +40,3 @@ export default function RootLayout({
     </ClerkProvider>
   );
 }
-  
